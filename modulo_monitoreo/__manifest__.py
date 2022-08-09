17daa20dd4f0f132915e87a304e41c3921470de3
Robin Cordova <robincordova7@gmail.om>
2022-08-08 20:04:30 -0700

    Creación del Modelo Estación

--- modulo_monitoreo/__manifest__.py
+++ modulo_monitoreo/__manifest__.py
@@ -13,9 +13,8 @@
     "version": "1.0",
     "depends": ["base", "mail"],
     "data": [
-        #"data/mail_template.xml",
-        #"data/tareas_programadas.xml",
-
-        #"views/views.xml",
+        "security/res_groups.xml",
+        "security/ir_model_access.xml",
+        "views/views.xml"
     ]
 }
---
+++ modulo_monitoreo/models/models.py
@@ -0,0 +1,25 @@
+from odoo import fields, models, api
+from odoo.exceptions import ValidationError
+
+
+class Estacion(models.Model):
+    _name = "mo.estacion"
+    _description = "Estación"
+
+    name = fields.Char(string="Nombre de la EStación", required=True)
+    temperatura = fields.Float(string="Temperatura")
+    humedad = fields.Float(string="Húmedad")
+    co2 = fields.Float(string="Nivel de CO2")
+    tvoc = fields.Float(string="TVOC")
+
+    url_servicio = fields.Char(string="Url del servicio", required=True)
+
+    validar = fields.Boolean(default=False, string="Validar Servicio")
+
+    estado = fields.Boolean(default=False, string="Estado")
+
+    _sql_constrainst = [
+        ('name_unique', 'unique (name)',
+         "El Nombre de Estación ya Existe")
+    ]
+
