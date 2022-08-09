7c9b313f5c45f1bf310756f55e04eed4e0a7f492
Robin Cordova <robincordova7@gmail.om>
2022-08-08 18:44:05 -0700

    Creación del módulo de software

---
+++ modulo_monitoreo/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Sistema de monitoreo de la calidad del Aire para el laboratorio de la Carrera de Ingeniería en Sistemas",
+    "description": "Módulo de software monitoreo de la calidad del Aire para el laboratorio de la Carrera de Ingeniería en Sistemas de la UNL",
+    "author": "Dario Gordillo",
+
+    "summary": """
+                Módulo de software monitoreo de la calidad del Aire para el laboratorio de la Carrera de Ingeniería en Sistemas de la UNL        """,
+
+    "website": "https://unl.edu.ec/",
+
+    "category": "Ambiente",
+
+    "version": "1.0",
+    "depends": ["base", "mail"],
+    "data": [
+        #"data/mail_template.xml",
+        #"data/tareas_programadas.xml",
+
+        #"views/views.xml",
+    ]
+}
